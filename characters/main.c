6a2bb47c97c4e62ab8ce4a1685e13c7e88200fc6
Kennedy Mwavu <mwavukennedy@gmail.com>
2025-08-25 10:46:20 +0300

    ft: intro to chars

---
+++ characters/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  char letter = 'A';
+  printf("fav letter is '%c', and in ascii it is '%d'\n", letter, letter);
+
+  char name[] = "mwavu";
+  printf("hello, %s!\n", name);
+  return 0;
+}
