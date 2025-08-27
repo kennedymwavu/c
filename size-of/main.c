3820fd7f5375535aa5e4ab6f11ae3728851f2028
Kennedy Mwavu <mwavukennedy@gmail.com>
2025-08-27 08:40:09 +0300

    ft: sizeof

---
+++ size-of/main.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+int main() {
+  printf("sizeof(char): %zu\n", sizeof(char));
+  printf("sizeof(char *): %zu\n", sizeof(char *));
+
+  printf("sizeof(char)   = %zu\n", sizeof(char));
+  printf("sizeof(bool)   = %zu\n", sizeof(bool));
+  printf("sizeof(int)   = %zu\n", sizeof(int));
+  printf("sizeof(float)   = %zu\n", sizeof(float));
+  printf("sizeof(double)   = %zu\n", sizeof(double));
+  printf("sizeof(size_t)   = %zu\n", sizeof(size_t));
+  return 0;
+}
