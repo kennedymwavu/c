0fd2f374023f3991b94c7c2118c01bfb4c2f4b2f
Kennedy Mwavu <mwavukennedy@gmail.com>
2025-08-28 22:34:16 +0300

    ft: multiples of 3 & 5

---
+++ project-euler/multiples-of-3-and-5/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int sum_multiples_of_3_and_5(int max) {
+  int total = 0;
+
+  for (int i = 1; i < max; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      continue;
+    }
+    total += i;
+  }
+
+  return total;
+}
+
+int main() {
+  int max = 1000;
+  int res = sum_multiples_of_3_and_5(max);
+  printf("res: %d\n", res);
+  return 0;
+}
