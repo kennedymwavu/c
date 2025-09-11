c7632bf7e0bbc26909ae90ab4ad48db28d49424e
Kennedy Mwavu <mwavukennedy@gmail.com>
2025-09-11 08:57:59 +0300

    ft: structs

--- structs/main.c
+++ structs/main.c
@@ -6,6 +6,24 @@ struct Person {
   float height;
 };
 
+struct Coordinate {
+  int x;
+  int y;
+  int z;
+};
+
+struct City {
+  char *name;
+  float lat;
+  float lon;
+};
+
+struct Coordinate new_coord(int x, int y, int z) {
+  struct Coordinate res = {.x = x, .y = y, .z = z};
+
+  return res;
+}
+
 int main() {
   printf("hello, world!\n");
 
@@ -16,6 +34,10 @@ int main() {
 
   printf("my name is %s. i'm %d years old, and %.2fcm in height.\n", mwavu.name,
          mwavu.age, mwavu.height);
+
+  struct City nairobi = {.name = "Nairobi", .lat = 78.5, .lon = 80.2};
+  printf("the name is %s located at (%f, %f)\n", nairobi.name, nairobi.lat,
+         nairobi.lon);
 
   return 0;
 }
